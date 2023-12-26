729c57f712cea3834706e0ea41230b28cee3bddb
pankaj ram pawar <pankajpawars123@gmail.com>
2023-12-26 23:04:20 +0530

    tokens stored to local storage for login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,15 +3,23 @@ import axios from "axios";
 import { loginValidation } from "../actions/authValidation";
 
 export default function Login() {
-  const handleLogin = async (formData) => {
+  const handleLogin = async (formData) => { 
     const validation = loginValidation(formData)
+    
     if (validation === 'validation passed') {
       try {
         const response = await axios.post(
           "http://localhost:8080/api/v1/auth/login",
           formData
         );
-        console.log(response);
+
+        const { accessToken, refreshToken, user, message} = response.data;
+        
+        localStorage.setItem("accessToken", accessToken);
+        localStorage.setItem("refreshToken", refreshToken); 
+        
+        alert(message);
+        console.log(message);
       } catch (error) {
         console.log(error);
       };
